47eb3d326a56464c657cb9467fa8b119c32987c6
Harry <68599167+harrykeeran12@users.noreply.github.com>
2022-04-27 08:39:38 +0100

    added ability for expression class to be recursive
    allowed recursive function in expression class

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import operator as op
 class Expression:
   'An Expression class, which takes in either an integer between 0-9 as the operands, or another instance of the expression class, as well as the operator.'
   def __init__(self, operand1, operator, operand2):
-    self.X = operand1
-    self.Y = operand2
+    if type(operand1) == Expression:
+      self.X = operand1.evaluate()
+    else:
+      self.X = operand1
+    if type(operand2) == Expression:
+      self.Y = operand2.evaluate()
+    else:
+      self.Y = operand2
     if operator == '+':
       self.operator = op.add
     elif operator == '-':
@@ -16,16 +22,23 @@ class Expression:
     self.type = (type(operand1), type(operand2))
   def is_valid(self):
     'Checks if the expression is valid.'
-    if (type(e.operator(e.X, e.Y)) is int) or (type(e.operator(e.X, e.Y)) is float):
+    if (type(self.operator(self.X, self.Y)) is int) or (type(self.operator(self.X, self.Y)) is float):
       return True
     else: 
       return False
   def evaluate(self):
     'Evaluates the operation in the instance.'
-    return e.operator(e.X, e.Y)
+    try:
+      return self.operator(self.X, self.Y)
+    except:
+      print('Error')
+
     
     
 
-e = Expression(4, '+', 5)
+e = Expression(6, '/', 2)
+f = Expression(e, '+', 5)
+
 print(e.evaluate())
 print(e.is_valid())
+print(f.evaluate())
